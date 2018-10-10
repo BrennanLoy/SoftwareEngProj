b67a9918455aa06a2d5a46066f113e6ba04661d9
COSLAB <COSLAB@134.154.250.228>
2018-10-10 12:05:15 -0700

    First steps.

---
+++ SoftEngProj/src/softengproj/SoftEngProj.java
@@ -0,0 +1,19 @@
+/*
+ * Brennan Loy, Yax Barillas, and David Peacock
+ */
+package softengproj;
+
+/**
+ *
+ * @author Brennan Loy
+ */
+public class SoftEngProj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
