1d616e3b4d6389e44930d6aa38ecc5bfdadde895
depeacock13 <depeacock13@aol.com>
2018-10-20 22:54:07 -0700

    Add files via upload

---
+++ src/Ticket2RideView/LPBonus.java
@@ -0,0 +1,17 @@
+/*  
+    David Peacock / SN8583
+    CS-401 Software Engineering
+    Due: Wed 24 Oct 2018 @ 11:59 PM
+    HW_4
+*/
+
+package ticket2rideview;
+
+public class LPBonus
+{
+    int points;
+    
+    // Constructor
+    LPBonus()
+    {   points = 10;    }
+}
